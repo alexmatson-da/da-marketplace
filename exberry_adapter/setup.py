f0629155ab6855fdfbf3e96ed5b0f2a4ead34c2a
Alex Matson <alex.matson@digitalasset.com>
2021-05-21 19:33:35 -0400

    bump 0.7.0

--- exberry_adapter/setup.py
+++ exberry_adapter/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='da-marketplace-exberry-adapter',
-      version='0.6.0',
+      version='0.7.0',
       description='Daml Marketplace Exberry Adapter',
       author='Digital Asset',
       url='daml.com',
